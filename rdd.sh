40aefe6703b26b4518f9af35853aaf33e6920677
lbb00 <mr@lbb00.com>
2024-10-11 18:41:33 +0800

    fix(rdd.sh): refactor variable usage to improve readability and maintainability fix(rdd.sh): redirect error messages to stderr for better error handling

--- rdd.sh
+++ rdd.sh
@@ -1,30 +1,31 @@
 #!/bin/bash
 
 rdd() {
-  if [[ -z $1 ]]; then
-    echo -e "\033[31m[rmf] Path is empty\033[0m"
+  local path="$1"
+
+  if [[ -z $path ]]; then
+    echo -e "\033[31m[rmf] Path is empty\033[0m" >&2
     return
   fi
 
-  if [[ $1 != */ ]]; then
-    1="${1}/"
+  if [[ $path != */ ]]; then
+    path="${path}/"
   fi
 
-  if [[ $1 == "/" ]]; then
-    echo -e "\033[31m[rmf] Deleting root directory is dangerous\033[0m"
+  if [[ $path == "/" ]]; then
+    echo -e "\033[31m[rmf] Deleting root directory is dangerous\033[0m" >&2
     return
   fi
 
-  if [[ ! -d $1 ]]; then
-    echo "rmf: $1 is not a directory"
+  if [[ ! -d $path ]]; then
+    echo "rmf: $path is not a directory" >&2
     return
   fi
 
-  echo "Are you sure you want to delete $1?"
+  echo "Are you sure you want to delete $path?"
   select yn in "Yes" "No"; do
     case $yn in
-      Yes )
-        break;;
+      Yes ) break;;
       No )
         echo "Cancelled"
         return;;
@@ -33,9 +34,10 @@ rdd() {
 
   rmf_empty_path="$HOME/rmf_empty/"
   mkdir -p "$rmf_empty_path"
-  rsync -av --delete "$rmf_empty_path" "$1"
+  rsync -av --delete "$rmf_empty_path" "$path"
   rm -rf "$rmf_empty_path"
-  rm -rf "$1"
+  rm -rf "$path"
 
-  echo -e "\033[32m[rmf] $1 is deleted\033[0m"
+  # 成功信息，使用标准输出
+  echo -e "\033[32m[rmf] $path is deleted\033[0m"
 }
